4010bbad31ada8e51443671545b40cd53c934a1e
Kyle Guthery <ksguthery@gmail.com>
2022-01-30 17:11:08 -0600

    logic

--- calculator.js
+++ calculator.js
@@ -1,64 +1,137 @@
 const buttons = document.getElementsByClassName("button");
 const display = document.getElementById("display");
+let numberOne;
+let numberTwo;
 let currentButton;
 let previousButton;
 let operatorButton;
+let answer;
+let remainingSpace;
 
-function math(currentButton, previousButton, operatorButton) {
-  let answer;
-  currentButton = parseFloat(currentButton);
-  previousButton = parseFloat(previousButton);
-
-  if (operatorButton == "+") {
-    answer = previousButton + currentButton;
-  } else if (operatorButton == "-") {
-    answer = previousButton - currentButton;
-  } else if (operatorButton == "/") {
-    answer = previousButton / currentButton;
-  } else if (operatorButton == "*") {
-    answer = previousButton * currentButton;
+function rounder(num) {
+  typeof(num);
+  let i = num.split('.')
+  let taken = i[0].length;
+  remainingSpace = 9 - taken;
+  if (i[1].length < remainingSpace) {
+    remainingSpace = i[1].length;
   }
-    let remainingSpace = answer.length-8;
-  display.innerText = answer.toFixed(remainingSpace);
 }
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", (e) => {
-    currentButton = buttons[i].dataset.button;
-    if (currentButton == "0" ||
-        currentButton == "1" ||
-        currentButton == "2" ||
-        currentButton == "3" ||
-        currentButton == "4" ||
-        currentButton == "5" ||
-        currentButton == "6" ||
-        currentButton == "7" ||
-        currentButton == "8" ||
-        currentButton == "9") {
-      if (display.innerText.length <= 8) {
-        display.innerText += currentButton;
-      }
-    } else if (currentButton == "." && !display.innerText.includes(".")) {
-      display.innerText += currentButton;
-    } else if (currentButton == "/" ||
-               currentButton == "*" ||
-               currentButton == "-" ||
-               currentButton == "+") {
-                previousButton = display.innerText;
-                display.innerText = "";
-                operatorButton = currentButton;
-                currentButton = "";
-              } else if (currentButton == "=") {
-                currentButton = display.innerText;
-                math(currentButton, previousButton, operatorButton)
-              } else if (currentButton == "clr") {
-                display.innerText = "";
-                currentButton = "";
-                previousButton = "";
-                operatorButton = "";
-              }
-    // console.log(`cb = ${currentButton}`);
-    // console.log(`pb = ${previousButton}`);
-    // console.log(`ob = ${operatorButton}`);
-  });
+function math(first, second, operatorButton) {
+  let answer;
+  first = parseFloat(first);
+  second = parseFloat(second);
+  if (operatorButton == "+") {
+    answer = first + second;
+  } else if (operatorButton == "-") {
+    answer = first - second;
+  } else if (operatorButton == "/") {
+    answer = first / second;
+  } else if (operatorButton == "*") {
+    answer = first * second;
+  }
+
+  if (answer.toString().length > 9 &&
+      answer % 1 == 0) {
+    answer = "error";
+    display.innerText = answer;
+  } else if (answer == Infinity) {
+    answer = "cut it out";
+    display.innerText = answer;
+  } else {
+    if (answer % 1 != 0) {
+      rounder(answer.toString());
+      display.innerText = answer.toFixed(remainingSpace);
+      numberOne = answer;
+      return answer;
+    } else {
+      display.innerText = answer;
+      numberOne = answer;
+      return answer;
+    }
+  }
 }
+
+function scrn(currentButton) {
+  if (currentButton == "." &&
+      display.innerText.includes(".")) {
+      return;  
+      }
+
+  if (numberOne == undefined &&
+      display.innerText.length < 9) {
+        if (display.innerText == "" &&
+            currentButton == "0") {
+            } else {
+              display.innerText += currentButton;
+            }
+    } else if (numberTwo == undefined) {
+        display.innerText += currentButton;
+      } else {
+          display.innerText = "";
+          display.innerText += currentButton;
+        }
+}
+
+function operator(currentButton) {
+  if (answer == undefined) {
+    if (numberOne == undefined) {
+      numberOne = display.innerText;
+      display.innerText = "";
+      operatorButton = currentButton;
+    } else {
+      numberTwo = display.innerText;
+      math(numberOne, numberTwo, operatorButton);
+      operatorButton = currentButton;
+    }
+  }
+}
+
+function calculator() {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", (e) => {
+      currentButton = buttons[i].dataset.button;
+      switch (currentButton) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+        case '.':
+          scrn(currentButton);
+          break;
+        case '+':
+          operator(currentButton);
+          break;
+        case '-':
+          operator(currentButton);
+          break;
+        case '*':
+          operator(currentButton);
+          break;
+        case '/':
+          operator(currentButton);
+          break;
+        case '=':
+          numberTwo = parseFloat(display.innerText);
+          math(numberOne, numberTwo, operatorButton);
+          break;
+        case 'clr':
+          numberOne = undefined;
+          numberTwo = undefined;
+          answer = undefined;
+          display.innerText = "";
+          break;
+      }
+    });
+  }
+}
+
+
+calculator();
